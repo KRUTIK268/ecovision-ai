8e31007bcd0a274a0b7d421db55a8d3c5ef5b010
KRUTIK268 <krutikparmar900@gmail.com>
2026-04-05 18:26:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,21 @@
 from ultralytics import YOLO
+import os
 
-# Load model (only once)
 model = YOLO("best.pt")
 
 def predict_image(image):
-    results = model.predict(image, verbose=False)
+    # Convert PIL → save temp file (no cv2 needed)
+    temp_path = "temp.jpg"
+    image.save(temp_path)
+
+    results = model.predict(temp_path, verbose=False)
 
     for r in results:
         probs = r.probs.data.tolist()
         names = r.names
 
-    # Get top 3 predictions
+    os.remove(temp_path)
+
     top3 = sorted(zip(names.values(), probs), key=lambda x: x[1], reverse=True)[:3]
 
     return top3
